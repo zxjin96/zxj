5944f05fd51f730a7ce921f6f797e405c8d9cc38
zyliu97 <zyliu18438655181@163.com>
2017-08-09 17:52:06 +0800

    update files

--- workbook/1-101.js
+++ workbook/1-101.js
@@ -1,12 +1,12 @@
-var union={
+var values={
       ch:undefined,
       n:undefined,
       f:undefined
 }
-union.ch="a";
-alert(union.ch);
-union.n=100;
-alert(union.n);
-union.f=78.168;
-alert(union.f);
+values.ch="a";
+alert(values.ch);
+values.n=100;
+alert(values.n);
+values.f=78.168;
+alert(values.f);
 
